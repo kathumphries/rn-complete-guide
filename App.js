7d27e0de487429803ebe1899f333ee9606fdf836
Kat Humphries <kat.humphries@joinroot.com>
2019-09-22 19:56:23 -0400

    removeGoalHandler and GoalItem onComplete props

--- App.js
+++ App.js
@@ -16,13 +16,23 @@ export default function App() {
     setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goalTitle}]);
   };
 
+  const removeGoalHandler = goalId => {
+      setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId );
+    });
+  }
+
   return (
     <View style= {styles.screen}>
       <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index)=> item.id}
         data={courseGoals}
-        renderItem = {itemData => <GoalItem onDelte = {() => console.log('Did that work?')} title = {itemData.item.value} />}
+        renderItem = {itemData =>
+        <GoalItem
+          id={itemData.item.id}
+          onComplete={removeGoalHandler}
+          title={itemData.item.value} />}
       />
     </View>
   );
